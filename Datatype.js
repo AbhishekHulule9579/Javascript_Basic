fb20bc50815af4d4302df0a31af992eb0ed5e3ca
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-08-26 11:20:05 +0530

    initial commit

---
+++ Datatype.js
@@ -0,0 +1,20 @@
+//Number Datatype
+// var a=1
+// console.log(a)
+// console.log(typeof(a))
+
+// var b=1111
+// console.log(b)
+// console.log(typeof(b))
+
+// var c=10.5
+// console.log(c)
+// console.log(typeof(c))
+
+var a='Abhishek'
+console.log(a)
+console.log(typeof(a))
+
+var b="Abhishek"
+console.log(b)
+console.log(typeof(b))
---
+++ variable.js
@@ -0,0 +1,37 @@
+// var a=12
+// document.write(a," ")
+
+// var b=4
+// document.write(b," ")
+
+// const c=3
+// document.write(c)
+{
+    var a=10
+    document.write(a)
+    a=20
+    document.write(a)
+}
+document.write(a)
+
+a=30
+document.write(a)
+document.write("<br>")
+{
+    let b=20
+    document.write(b)
+    b=6
+    document.write(b)
+}
+//document.write(b)  //outside the bracket it's not accessible
+// b=10
+// console.log(b)
+// c=11
+// console.log(c)
+{
+    const c=8
+    console.log(c)
+    // c=9
+    // console.log(c)
+}
+//console.log(c)//cannot accsess the variable outside the bracket/scope "const"
