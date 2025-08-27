e37d76ac3b4094a28e6cefea49bf7c4863a18dcb
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-08-27 17:01:49 +0530

    Initial Commit

--- operator/operators.js
+++ operator/operators.js
@@ -55,3 +55,79 @@ var a1=10
 var b1=20
 a1+=b1
 console.log("a1 += b1 =",a1)
+
+var a2=10
+var b2=20
+a2-=b2
+console.log("a2 += b2 =",a2)
+
+var a3=10
+var b3=20
+a3*=b3
+console.log("a3 *= b3 =",a3)
+
+var a4=10
+var b4=20
+a4/=b4
+console.log("a4 /= b4 =",a4)
+
+var a5=10
+var b5=20
+a5**=b5
+console.log("a5 **= b5 =",a5)
+
+var a6=10
+var b6=20
+console.log("a6 =b6 =",a6=b6)
+
+// Comparison Operators
+console.log("Comparison Operators")
+
+var a=10
+var b=20
+console.log("a == b =",a==b)
+
+var a1=10
+var b1=20
+console.log("a1 > b1 =",a1>b1)
+
+
+var a1=10
+var b1=20
+console.log("a1 < b1 =",a1<b1)
+
+var a3=10
+var b3=20
+console.log("a3 >= b3 =",a3>=b3)
+
+var a4=10
+var b4=20
+console.log("a4 <= b4 =",a4<=b4)
+
+var a5=10
+var b5=20
+console.log("a5 != b5 =",a5!=b5)
+
+//String Operator
+
+console.log("String Operator")
+
+var str1="Hello"
+var str2="World"
+var result=str1+" "+str2
+console.log("String Concatenation =",result)
+
+//Logical Operators
+
+console.log("Logical Operators")
+var a=10
+var b=20
+console.log((a==b)&& (a<b))
+
+var a=10
+var b=20
+console.log((a==b)|| (a<b))
+
+var a=10
+var b=20
+console.log(a!=b)
