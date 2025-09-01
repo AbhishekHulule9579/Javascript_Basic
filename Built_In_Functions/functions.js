bcaeca335b03992e75b2cb7e120d94322bf60d59
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-09-01 20:04:29 +0530

    Initial Commit

--- Built_In_Functions/functions.js
+++ Built_In_Functions/functions.js
@@ -1,32 +1,32 @@
 //Built in function
 //Alert function
 
-// var str1="hello"
-// var str2="world"
-// alert(str1+str2)
+var str1="hello"
+var str2="world"
+alert(str1+str2)
 
 //Confirm function
-// var a=10
-// var b=20
-// confirm(a+b)
+var a=10
+var b=20
+confirm(a+b)
 
 //Prompt function
-// var str1= prompt("Enter your first name")
-// var str2=prompt("Enter your last name")
-// console.log(str1+" "+str2)
+var str1= prompt("Enter your first name")
+var str2=prompt("Enter your last name")
+console.log(str1+" "+str2)
 
-// var a=parseInt(prompt("Enter a first number"))
-// var b=parseInt(prompt("Enter a second number"))
-// console.log(a+b)
+var a=parseInt(prompt("Enter a first number"))
+var b=parseInt(prompt("Enter a second number"))
+console.log(a+b)
 
-// var a=parseFloat(prompt("Enter a first number"))
-// var b=parseFloat(prompt("Enter a second number"))
-// console.log(a+b)
+var a=parseFloat(prompt("Enter a first number"))
+var b=parseFloat(prompt("Enter a second number"))
+console.log(a+b)
 
 //Square root function
-// var a=parseInt(prompt("Enter a first number"))
-// var b=Math.sqrt(a)
-// console.log(b)
+var a=parseInt(prompt("Enter a first number"))
+var b=Math.sqrt(a)
+console.log(b)
 
 //User Defined function
 //Function Declaration
