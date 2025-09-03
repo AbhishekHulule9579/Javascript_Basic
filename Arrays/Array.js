5a06577241653ea188305d73c8e6859023d11dd5
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-09-03 10:40:10 +0530

    Initial Commit

---
+++ Arrays/Array.js
@@ -0,0 +1,14 @@
+// creation fo Array
+var arr=[1,2,3,4,5]
+console.log(arr)
+
+var arr1=[]
+arr1[0]="A"
+arr1[1]="B"
+arr1[2]="C"
+arr1[3]="D"
+arr1[4]="E"
+console.log(arr1)
+
+var arr2=new Array(1,2,3,4,5)
+console.log(arr2)
