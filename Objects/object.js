53799de2e8ee5c7db3b8ce41aaba6e066ff3f212
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-09-04 16:05:13 +0530

    Initial Commit

---
+++ Objects/object.js
@@ -0,0 +1,23 @@
+//Creation of objects
+
+var object={
+    fname:"Abhishek",
+    lname:"Hulule",
+    age:21,
+    dob:2/7/2003
+}
+console.log(object);
+
+//Another way of creating object
+var obj={}
+obj.fname="Abhishek"
+obj.lname="Hulule"
+
+console.log(obj);
+console.log(obj.fname);
+
+//Another way of creating object
+var ob=new Object();
+ob.fname="Abhishek"
+ob.lname="Hulule"
+console.log(ob);
