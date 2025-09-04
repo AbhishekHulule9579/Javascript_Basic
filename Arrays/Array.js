ffc30a28e7ac110d543d68fb6d30c4979d38eac9
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-09-04 15:49:44 +0530

    initial Commit

--- Arrays/Array.js
+++ Arrays/Array.js
@@ -12,3 +12,54 @@ console.log(arr1)
 
 var arr2=new Array(1,2,3,4,5)
 console.log(arr2)
+
+var arr3=[1,2,3,4,5,6]
+console.log(arr3)    
+
+//Add element in array end
+arr3.push(7)
+console.log(arr3)
+
+//Add element in array start
+arr3.unshift(11)
+console.log(arr3)
+
+//Add element in array middle
+//But it does not shift further
+arr3[3]=33
+console.log(arr3)
+
+//Remove element from array end
+arr3.pop()
+console.log(arr3)
+
+//Remove element from array start
+arr3.shift()
+console.log(arr3)
+
+//delete element from array middle
+//It does not shift further
+//It leaves undefined hole in place of deleted element
+delete arr3[2]
+console.log(arr3)
+
+//Remove element from array middle and shift further
+arr3.splice(2,1)
+console.log(arr3)
+
+//length of array
+console.log(arr3.length)
+
+//sorting of array
+arr3.unshift(9)
+console.log(arr3)
+arr3.sort()
+console.log(arr3)
+
+//reverse of array
+arr3.reverse()
+console.log(arr3)
+
+//slice of array
+ar4=arr3.slice(2,4)
+console.log(ar4)
