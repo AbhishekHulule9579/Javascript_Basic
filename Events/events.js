ce2accf0a5394d90cbd14b2788510c6f87dcd707
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-09-05 14:04:37 +0530

    Initial commit

---
+++ Events/events.js
@@ -0,0 +1,9 @@
+function display(){
+    alert("this is onclick event");
+}
+
+function display(inpt){
+    confirm("the modified name is: "+inpt)
+}
+
+document.getElementById("msg").innerHTML="this is h1 tag."
